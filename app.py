619e18eb62e89ed923d6fd61e1669b192939399e
wanjohidaniel <162096534+wanjohidaniel@users.noreply.github.com>
2024-08-16 09:46:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -440,5 +440,6 @@ api.add_resource(Reject, '/reject/<int:id>')
 api.add_resource(Delete, '/delete/<int:id>')
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=5000)
